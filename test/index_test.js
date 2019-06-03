4b424e55d04f5baa89cae75a9ad1bd9fc03a0a7b
Simon Taylor <nizmox@gmail.com>
2019-06-03 16:15:00 +1000

    more tests

--- test/index_test.js
+++ test/index_test.js
@@ -1,11 +1,28 @@
 const test = require('ava');
 const unique = require('../index');
 
-test('must correctly filter unique values', (t) => {
+test('must correctly filter unique numbers', (t) => {
   const arr = [1,2,3,4,1,3,5,8,9,9];
 
   const result = arr.filter(unique);
 
   const expected = [1, 2, 3, 4, 5, 8, 9];
   t.deepEqual(result, expected);
+});
+
+test('must correctly filter unique strings', (t) => {
+  const arr = ['Mark', 'Steve', 'Bernie', 'Mark', 'Harry', 'Joe', 'Harry', 'Mark'];
+
+  const result = arr.filter(unique);
+
+  const expected = ['Mark', 'Steve', 'Bernie', 'Harry', 'Joe'];
+  t.deepEqual(result, expected);
+});
+
+test('must not mess with types that cannot be compared', (t) => {
+  const arr = [{}, [1,2,3], { awesome: true }, []];
+
+  const result = arr.filter(unique);
+
+  t.deepEqual(result, arr);
 });
